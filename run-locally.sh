2c47d8dd2564deeac832d62a3d999c6b300993ee
Tamas K. Papp <tkpapp@gmail.com>
2018-01-17 13:21:21 +0100

    Testing nightly instead.
    This also tests the download script.

--- run-locally.sh
+++ run-locally.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# test Docker build locally
+
 TMPDIR=`mktemp -d`
+echo "making a local copy into $TMPDIR for testing"
 cp -a /home/tamas/src/julia-local-packages/PGFPlotsX.jl/. $TMPDIR/
-docker run -ti -a STDOUT -a STDIN -a STDERR -v $TMPDIR:/mnt texlive-julia-minimal /test/test-script.sh stable PGFPlotsX
+docker run -t -a STDOUT -a STDIN -a STDERR -v $TMPDIR:/mnt texlive-julia-minimal /test/test-script.sh nightly PGFPlotsX

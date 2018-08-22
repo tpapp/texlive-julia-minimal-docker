7a90ce830d8fc59dc1a404b230a7e030a812de49
Tamas K. Papp <tkpapp@gmail.com>
2018-08-22 11:23:04 +0200

    Fix cirrus build.

---
+++ docker-push-script.sh
@@ -0,0 +1,13 @@
+USER=tkpapp
+IMAGE=texlive-julia-minimal
+
+set -e
+
+if [ "$CIRRUS_BRANCH" != "master" ]
+then
+    exit 0
+fi
+
+docker login --username $DOCKER_USER --password $DOCKER_PASSWORD
+
+docker push $USER/$IMAGE

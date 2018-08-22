e9c996b1749c8d37497b92d90c5765a1053e7567
Tamas K. Papp <tkpapp@gmail.com>
2018-08-22 13:20:20 +0200

    add test plot

--- test-script-inner.sh
+++ test-script-inner.sh
@@ -3,3 +3,5 @@ set -e
 /test/julia-1.0/bin/julia -e '1+1'
 /test/install-julia.sh nightly
 /test/julia-nightly/bin/julia -e '1+1'
+pdflatex /test/test-plot.tex
+test -e /test/test-plot.pdf || exit 1

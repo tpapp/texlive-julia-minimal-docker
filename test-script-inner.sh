1e97f3c5df311305c3a8245493b2bc07129af1ea
Tamas K. Papp <tkpapp@gmail.com>
2018-08-22 13:39:23 +0200

    Fix output directory.

--- test-script-inner.sh
+++ test-script-inner.sh
@@ -6,5 +6,5 @@ set -e
 /test/julia-1.0/bin/julia -e '1+1' # should be installed
 /test/install-julia.sh nightly     # installation should work
 /test/julia-nightly/bin/julia -e '1+1' # installed nightly should work
-pdflatex -halt-on-error /mnt/test-plot.tex # latex environment should be available ...
+pdflatex -output-directory /mnt -halt-on-error /mnt/test-plot.tex # latex environment should be available ...
 test -e /mnt/test-plot.pdf || exit 1  # ... and produce a PDF

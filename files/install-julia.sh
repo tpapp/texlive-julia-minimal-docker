115dca8c487622db672e940de3331d399e26108a
Robert Schwarz <mail@rschwarz.net>
2018-10-11 09:21:35 +0200

    update to 0.6.4, fix error msg

--- files/install-julia.sh
+++ files/install-julia.sh
@@ -18,7 +18,7 @@ VERSION=$1
 
 case $VERSION in
     0.6)
-        URL=https://julialang-s3.julialang.org/bin/linux/x64/0.6/julia-0.6.2-linux-x86_64.tar.gz
+        URL=https://julialang-s3.julialang.org/bin/linux/x64/0.6/julia-0.6.4-linux-x86_64.tar.gz
         ;;
     1.0)
         URL=https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.1-linux-x86_64.tar.gz
@@ -27,7 +27,7 @@ case $VERSION in
         URL=https://julialangnightlies-s3.julialang.org/bin/linux/x64/julia-latest-linux64.tar.gz
         ;;
     *)
-        echo "Unknown Julia version '$1'. Use '0.6' or 'nightly'."
+        echo "Unknown Julia version '$1'. Use '1.0', '0.6' or 'nightly'."
         exit 1
 esac
 

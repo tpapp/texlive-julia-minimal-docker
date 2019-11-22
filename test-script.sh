d086f968a7f710152ea311174c7ad3f3f74b4d3e
Tamas K. Papp <tkpapp@gmail.com>
2019-11-22 10:00:11 +0100

    provide defaults if not set

--- test-script.sh
+++ test-script.sh
@@ -2,4 +2,4 @@
 
 # This script is run by the CI environment.
 
-docker run -t -a STDOUT -a STDIN -a STDERR tkpapp/texlive-julia-minimal /test/latex-tests/test-script-inner.sh
+docker run -t -a STDOUT -a STDIN -a STDERR $DOCKER_USER/$IMAGE /test/latex-tests/test-script-inner.sh

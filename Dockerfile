5e79cd3741b81b25c8dee2e993047b7b5e88698f
Tamas K. Papp <tkpapp@gmail.com>
2019-12-30 08:34:04 +0100

    test with more recent PGFPlotsX config, add relevant package

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ ADD files/latex-tests/ /test/latex-tests/
 
 RUN apt-get update \
         && apt-get install --no-install-recommends -qq texlive-latex-base git \
-        texlive-pictures texlive-latex-extra pdf2svg \
+        texlive-luatex texlive-pictures texlive-latex-extra pdf2svg \
         poppler-utils gnuplot-nox wget ca-certificates openssh-client rsync file \
         && /test/install-julia.sh 1.2 \
         && chmod 700 /root/.ssh && chmod 600 /root/.ssh/*
--- files/latex-tests/test-script-inner.sh
+++ files/latex-tests/test-script-inner.sh
@@ -25,15 +25,12 @@ echo "########################################"
 echo "#### testing pdflatex"
 echo "########################################"
 
-pdflatex -halt-on-error test-plot.tex
-file -bEi test-plot.pdf | grep "application/pdf"
-cat test-plot.log
-rm test-plot.log test-plot.pdf
+pdflatex -halt-on-error test-pdflatex.tex
+file -bEi test-pdflatex.pdf | grep "application/pdf"
 
 echo "########################################"
 echo "#### testing lualatex"
 echo "########################################"
 
-lualatex --halt-on-error test-plot.tex
-file -bEi test-plot.pdf | grep "application/pdf"
-rm test-plot.tex
+lualatex --halt-on-error test-lualatex.tex
+file -bEi test-lualatex.pdf | grep "application/pdf"
